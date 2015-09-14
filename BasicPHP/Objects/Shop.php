594cbb87bf9191e49faa50d66344b3deef09c6c4
orny <tasnim765@gmail.com>
2015-09-14 23:45:59 +0600

    Adding project  files

---
+++ BasicPHP/Objects/Shop.php
@@ -0,0 +1,56 @@
+<?php
+class User {
+	private $shop_id;
+	private $shop_name;
+	private $shop_address;
+	private $shop_description;
+	private $shop_type_id;
+	private $date_time;
+	private $user_id;
+	
+	
+	public function setShopID($shop_id){
+		$this->shop_id=$shop_id;
+	}	
+	public function getShopID(){
+		return $this->shop_id;
+	}
+	
+	public function setShopName($shop_name){
+		$this->shop_name=$shop_name;
+	}
+	public function getShopName(){
+		return $this->shop_name;
+	}
+	
+	public function setSAddress($shop_address){
+		$this->shop_address=$shop_address;
+	}
+	public function getSAddress(){
+		return $this->shop_address;
+	}
+	
+	public function setSTid($shop_type_id){
+		$this->shop_type_id=$shop_type_id;
+	}
+	public function getSTid(){
+		return $this->shop_type_id;
+	}
+	
+	public function setDate($date_time){
+		$this->date_time=$date_time;
+	}
+	public function getDate(){
+		return $this->date_time;
+	}
+	
+	public function setUserID($user_id){
+		$this->user_id=$user_id;
+	}	
+	public function getUserID(){
+		return $this->user_id;
+	}
+	
+	
+}
+?>
---
+++ BasicPHP/Objects/User.php
@@ -0,0 +1,50 @@
+<?php
+class User {
+	private $user_id;
+	private $uer_name;
+	private $user_pass;
+	private $user_email;
+	private $date_time;
+	
+	public function setUserID($user_id){
+		$this->user_id=$user_id;
+	}	
+	public function getUserID(){
+		return $this->user_id;
+	}
+	
+	public function setUserName($uer_name){
+		$this->user_name=$uer_name;
+	}
+	public function getUserName(){
+		return $this->user_name;
+	}
+	
+	/*Please complete the coding for other private variables*/
+	/*After that in the same way write class for Shop*/
+	
+	public function setPass($uer_pass){
+		$this->user_pass=$user_pass;
+	}
+	public function getPass(){
+		return $this->user_pass;
+	}
+	
+	public function setEmail($uer_email){
+		$this->user_email=$user_email;
+	}
+	public function getEmail(){
+		return $this->user_email;
+	}
+	
+	public function setDate($date_time){
+		$this->date_time=$date_time;
+	}
+	public function getDate(){
+		return $this->date_time;
+	}
+	
+	
+	
+}
+?>
