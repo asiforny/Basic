e4b0126b925d10cdb46f93052c28ee6ce95a25a4
orny <tasnim765@gmail.com>
2015-10-20 15:44:18 +0600

    data access commit

---
+++ snapdeal/DataAccess/DataaccessHelper.php
@@ -0,0 +1,17 @@
+<?php
+class DataAccessHelper{
+	public static function ExecuteInsertReturnID($object){
+		/*
+		/ You have to implement it
+		*/
+		
+	}
+	
+	public static function ExecuteQuery($object){
+		/*
+		/ You have to implement it
+		*/
+		
+	}
+}
+?>
