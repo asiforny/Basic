84584876ebc5a05aa2776d3bd4a14fbc191d82d8
sohel364 <sohel.official@gmail.com>
2015-10-26 01:11:31 +0600

    Manager/clsProductsManager.php updates

--- snapdeal/Manager/clsProductsManager.php
+++ snapdeal/Manager/clsProductsManager.php
@@ -9,7 +9,7 @@ class clsProductsManager{
 		$DataBaseHelper=new DataAccessHelper();
 		$sql="INSERT INTO `products` (`product_name`, `product_price`, `discount`,`emi_availablity`,`emi_rate`,`type`,`capacity_`,`key_features`,`image`,`category_name`,`sub_category_name`) VALUES ('".$ProductsObject->GetProductName()."','".$ProductsObject->getProductPrice()."', '".$ProductsObject->Getdiscount()."','".$ProductsObject->GetEMIAV()."','".$ProductsObject->GetEMIR()."','".$ProductsObject->GetType()."','".$ProductsObject->GetCapacity()."','".$ProductsObject->GetKeyf()."','".$ProductsObject->GetImage()."','".$ProductsObject->GetCatN()."','".$ProductsObject->GetSCatN()."')";  
 		
-				echo $sql;
+		echo $sql;
 		return $DataBaseHelper->ExecuteInsertReturnID($sql);
 	}
 	
