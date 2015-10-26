995de18ef8e3a581dae17da5aa2570069705c9fd
orny <tasnim765@gmail.com>
2015-10-27 00:11:42 +0600

    commit

--- snapdeal/Manager/clsProductsManager.php
+++ snapdeal/Manager/clsProductsManager.php
@@ -8,7 +8,7 @@ class clsProductsManager{
 	
 	public static function InsertProducts($ProductsObject){
 		$DataBaseHelper=new DataAccessHelper();
-		$sql="INSERT INTO `products` (`product_id`,`product_name`, `product_price`, `dis_percentange`,`before_dis_price`,`emi_availablity`,`emi_rate`,`features`,`image`,`category_name`,`sub_category_name`) VALUES ('".$ProductsObject->GetProductID() ."','".$ProductsObject->GetProductName()."','".$ProductsObject->getProductPrice()."', '".$ProductsObject->Getdiscount()."','".$ProductsObject->Getbfdiscount()."','".$ProductsObject->GetEMIAV()."','".$ProductsObject->GetEMIR()."','".$ProductsObject->Getfeatures()."','".$ProductsObject->GetImage()."','".$ProductsObject->GetCatN()."','".$ProductsObject->GetSCatN()."')";  
+		$sql="INSERT INTO `products` (`product_id`,`product_name`, `product_price`, `dis_percentange`,`before_dis_price`,`emi_availablity`,`emi_rate`,`features`,`image`,`category_name`,`sub_category_name`) VALUES ('".$ProductsObject->getProductId() ."','".$ProductsObject->GetProductName()."','".$ProductsObject->GetProductPrice()."', '".$ProductsObject->GetDiscountDetails()."','".$ProductsObject->GetProductBeforeDiscounePrice()."','".$ProductsObject->GetEmi()."','".$ProductsObject->GetEmiDetails()."','".$ProductsObject->GetProductFeatures()."','".$ProductsObject->GetImage()."','".$ProductsObject->GetProductCategory()."','".$ProductsObject->GetProduSetProductSubCategoryct()."')";  
 		
 		echo $sql;
 		return $DataBaseHelper->ExecuteInsertReturnID($sql);
