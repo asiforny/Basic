ffd32a0c0b04dcaf46b925ef27725f165f12cbc1
orny <tasnim765@gmail.com>
2015-10-27 11:56:16 +0600

    new commit

--- snapdeal/Manager/clsProductsManager.php
+++ snapdeal/Manager/clsProductsManager.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(0);
-include_once('../objects/clsProducts.php');
+include_once('../objects/clsProduct.php');
 include_once('../DataAccess/DataaccessHelper.php');
 class clsProductsManager{
 	public $DataBaseHelper=NULL;
