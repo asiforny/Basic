9446b6230b729079620823cc4a87d37b34a3925c
Mahmudul Hasan <sohel.official@gmail.com>
2015-11-12 01:17:34 +0600

    mysql saf command fix
    using mysql_escape_string

--- snapdeal/Manager/clsProductsManager.php
+++ snapdeal/Manager/clsProductsManager.php
@@ -11,9 +11,17 @@ class clsProductsManager{
                 echo '</pre>';
                 //echo "i am here";
 		$DataBaseHelper=new DataAccessHelper();
-		$sql="INSERT INTO `products` (`product_id`,`product_name`, `product_price`, `dis_percentange`,`before_dis_price`,`emi_availablity`,`emi_rate`,`features`,`img_url`,`category_name`,`sub_category_name`,`product_details_url`,`product_rating`) VALUES ('".NULL ."','".$ProductsObject->GetProductName()."','".$ProductsObject->GetProductPrice()."', '".$ProductsObject->GetDiscountDetails()."','".$ProductsObject->GetProductBeforeDiscounePrice()."','".$ProductsObject->GetEmi()."','".$ProductsObject->GetEmiDetails()."','".$ProductsObject->GetProductFeatures()."','".$ProductsObject->GetImgUrl()."','".$ProductsObject->GetProductCategory()."','".$ProductsObject->GetProductSubCategory()."','".$ProductsObject->GetProductDetailUrl()."','".$ProductsObject->GetProductRating()."')";  		
+		$sql="INSERT INTO `products` (`product_id`,`product_name`, `product_price`, `dis_percentange`,`before_dis_price`,`emi_availablity`,`emi_rate`,`features`,`img_url`,`category_name`,`sub_category_name`,`product_details_url`,`product_rating`) VALUES ('".NULL ."','".safe($ProductsObject->GetProductName())."','".safe($ProductsObject->GetProductPrice())."', '".safe($ProductsObject->GetDiscountDetails())."','".safe($ProductsObject->GetProductBeforeDiscounePrice())."','".safe($ProductsObject->GetEmi())."','".safe($ProductsObject->GetEmiDetails())."','".safe($ProductsObject->GetProductFeatures())."','".safe($ProductsObject->GetImgUrl())."','".safe($ProductsObject->GetProductCategory())."','".safe($ProductsObject->GetProductSubCategory())."','".safe($ProductsObject->GetProductDetailUrl())."','".safe($ProductsObject->GetProductRating())."')";  		
 		echo $sql."<br/>";
 		return $DataBaseHelper->ExecuteInsertReturnID($sql);
 	}	
 }
+
+
+
+
+function safe($value){
+	return mysql_real_escape_string($value);
+}
+
 ?>
