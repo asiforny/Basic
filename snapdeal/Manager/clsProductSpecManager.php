78842c6fec9096c8d98395b0b180d0ffeaf8ac3c
orny <tasnim765@gmail.com>
2015-12-20 00:15:06 +0600

    new commit

--- snapdeal/Manager/clsProductSpecManager.php
+++ snapdeal/Manager/clsProductSpecManager.php
@@ -14,10 +14,34 @@ class clsProductSpecManager{
 		$sql="INSERT INTO `product_spec` (`id`,`product_id`, `product_spec_type`, `product_data`) VALUES ('".NULL ."','".safe($ProductsObject->GetProductID())."','".safe($ProductsObject->GetProductSpecType())."', '".safe($ProductsObject->GetProductData())."')";  		
 		//echo $sql."<br/>";
 		return $DataBaseHelper->ExecuteInsertReturnID($sql);
-	}	
+		
+		
+		
+	}
+
+	
+	public static function GetAllProductURL($TableName){
+		
+		$sql="SELECT `product_id`,`product_name`,`product_price`,`dis_percentange`,`before_dis_price`,`emi_availablity`,`emi_rate`,`features`,`img_url`,`category_name`,`sub_category_name`,`product_details_url`,`product_rating` FROM " ."'".$TableName."'"; //`isCrawled`='0' LIMIT 13101,33929";
+ echo $sql;
+ 
+ return ;
+ 
+ $DataBaseHelper=new DataAccessHelper();
+ $dataset=$DataBaseHelper->ExecuteDataSet($sql);
+ 
+ echo "here i am";
+ echo "<pre>";
+ print_r($dataset);
+ echo "<pre>";
+			
+	}
+	
 }
 
 
+
+clsProductSpecManager::GetAllProductURL("antiques_&_collectibles");
 
 
 function safe($value){
