ca8fc543a028eb88b73412a76adee29fe4e8c1c0
sohel364 <sohel.official@gmail.com>
2016-02-29 00:36:35 +0600

    spec improvements

--- snapdeal/app/spec_crawler.php
+++ snapdeal/app/spec_crawler.php
@@ -6,7 +6,7 @@ include_once ('../Manager/clsProductSpecManager.php');
 
 
 
-$url="file:///C:/Users/simon/Desktop/try.html";
+$url="file:///C:/Users/simon/Desktop/try1.html";
 $innerHtml = file_get_html($url);
 
 $ProductArray=NULL;
@@ -36,29 +36,58 @@ foreach($innerHtml->find('div[class=comp comp-product-specs]') as $mitem)
 
         foreach ($item->find('div[class=spec-body]') as $item_body){
 
-        if($item_body->find('ul')){
+        if($item_body->find('ul'))
+		{
                                     $type_list_data_combined="";
             
                                       foreach ($item_body->find('ul li') as $item_spec_li_data) {
                                                   //echo "This list data: ". $item_spec_li_data;
                                                   $type_list_data_combined.=$item_spec_li_data->plaintext .",";
-                                             }
-                                          
+                                             }                                          
                                           //echo "List Combined data : ". $type_list_data_combined;     
                                           $SingleProduct->SetProductData(rtrim($type_list_data_combined, ","));                                        
-                                    }
-          else {
-            //echo "this is full tex :".$item_body;
-            $SingleProduct->SetProductData($item_body->plaintext);
-          }
+		}
+        else {
+              //echo "this is full tex :".$item_body;              
+              if($item_body->find('tbody table'))
+              {
+                //$SingleProduct->SetProductData($item_body->plaintext);
+				$type_list_data_combined2="";
+				foreach($item_body->find('table[class=product-spec]') as $specInnerType)
+				{		 //echo $specInnerType."<br>";
+						 $specTypeStringCollector = "";
+						 $specTypeStringCollector.=$specInnerType->find("tr",0)->plaintext." :";
+						 //echo $specInnerType->find('tr th')->plaintext.'<br>';
+																			 
+						 foreach($specInnerType->find('tr') as $Typevalue)
+						 {
+							$pairString = "";
+							//echo $Typevalue.'<br>';
+							//$i=0;
+							$pairString.= trim($Typevalue->find("td",0)->plaintext).":".trim($Typevalue->find("td",1)->plaintext.',');
+							
+							/*foreach($Typevalue->find('td') as $Perl)
+							{
+								 if($i>0)
+								 {$pairString.=":";}
+								 
+								 $pairString.= trim($Perl);
+								 $i++;
+							}*/
+							$specTypeStringCollector.=$pairString;
+						 }
+						
+						$type_list_data_combined2.=trim($specTypeStringCollector,',');
+				}				
+				//echo $type_list_data_combined2.'<br>';
+				$SingleProduct->SetProductData(trim($type_list_data_combined2, ','));
+              }
+            }		
         }
-
         echo "<pre>";
           print_r($SingleProduct);
         echo "</pre>";
         echo "<hr></br>";
-
-
          
        $DataAccess = new DataaccessHelper();     
        clsProductSpecManager::InsertProducts($SingleProduct);
